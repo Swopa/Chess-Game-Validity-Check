3baf504f57a39f95341253e550707b719513722f
kiu_tepnadze.nikoloz <tepnadze.nikoloz@kiu.edu.ge>
2025-04-14 22:19:44 +0400

    check logic for king (castling still to be implemented)

--- src/King.java
+++ src/King.java
@@ -5,13 +5,14 @@ public class King extends Piece{
 
     @Override
     boolean isValidMove(int srcRow, int srcCol, int destRow, int destCol, Piece[][] board) {
-        //TODO check logic | Castling
-
-
+        //TODO check logic
         //King moves one space in any direction
         int rowDiff = Math.abs(srcRow-destRow);
         int colDiff = Math.abs(srcCol-destCol);
 
+        if(underCheck(destRow, destCol, board)){
+            return false;
+        }
 
         if((rowDiff <= 1 && colDiff <= 1) && (rowDiff + colDiff) != 0){
             Piece destPiece = board[destRow][destCol];
@@ -19,6 +20,29 @@ public class King extends Piece{
             return destPiece == null || destPiece.isWhite != this.isWhite;
         }
 
+        return false;
+    }
+
+    boolean underCheck(int destRow, int destCol, Piece[][] board){
+        for(int row = 0; row < 8; row++){
+            for(int col = 0; col < 8; col++){
+                Piece piece = board[row][col];
+                if(piece != null && piece.isWhite != this.isWhite){
+                    if(piece.isValidMove(row, col, destRow, destCol, board)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    boolean shortCastling(){
+        return false;
+    }
+
+    boolean longCastling(){
         return false;
     }
 }
